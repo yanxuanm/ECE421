59ad34932a4ec4bc538015125c816a91b769a17c
ElizaWcq <33061281+ElizaWcq@users.noreply.github.com>
2019-01-15 22:15:19 -0500

    Update gradMSE.py

--- A1/gradMSE.py
+++ A1/gradMSE.py
@@ -3,4 +3,5 @@ def gradMSE(W, b, x, y, reg):
     transpose_W = np.transpose(W)
     error = np.matmul(transpose_W, x) + b - y
     grad_mse = (np.sum(np.transpose(error)* x))/(np.shape(y)[0]) + 2 * reg * W
+    "Test"
     return grad_mse

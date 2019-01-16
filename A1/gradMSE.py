b581eb71ad07badf11d01a4f4d61e93aa52f5ed7
joshua <joshuaktf@gmail.com>
2019-01-15 22:14:47 -0500

    delete addtional python file

--- A1/gradMSE.py
+++
@@ -1,6 +0,0 @@
-def gradMSE(W, b, x, y, reg):
-    # Your implementation here
-    transpose_W = np.transpose(W)
-    error = np.matmul(transpose_W, x) + b - y
-    grad_mse = (np.sum(np.transpose(error)* x))/(np.shape(y)[0]) + 2 * reg * W
-    return grad_mse
